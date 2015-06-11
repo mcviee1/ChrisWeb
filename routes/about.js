81ddbe4226bd08d962793798cebf4d4423045a94
mcviee1 <mcviee1@hotmail.com>
2015-06-12 00:10:36 +0100

    Updated About Page

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/about.json');
 
 /* GET About Me page. */
 router.get('/', function(req, res) {
-  res.render('aboutme');
+  res.render('about', data);
 });
 
 module.exports = router;
